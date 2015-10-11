ea78563512fa32c6fde08fc79ec7fbd871d343e5
Evangeline Liu <eliu@MacBook-Pro-7.local>
2015-10-11 16:18:53 -0500

    display menu now yay

--- ServerySign/ServerySign/urls.py
+++ ServerySign/ServerySign/urls.py
@@ -26,20 +26,21 @@ def simplePrint(request):
     A very simple page that just renders to test url routing
     """
     foodList = getServeryData()
-    foodstr = ''
-    for food in foodList:
-        foodstr = foodstr+food
-    return HttpResponse(foodstr)
+    foodString = "<br />".join(foodList)
+    return HttpResponse(foodString)
 
 def getServeryData():
     response = urllib2.urlopen('http://dining.rice.edu')
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
     foodList = []
-    for item in soup.findAll("div", class_="servery-title"):
-        foodList.append(item.get_text())
-        foodList.append(item.next_sibling.next_sibling.get_text())
-    return foodList
+    for servery in soup.findAll("div", class_="servery-title"):
+    	if (servery.get_text() == "\nWest\n"):
+    		unorderedList = servery.next_sibling.next_sibling
+    		for item in unorderedList.findAll('li'):
+    			foodList.append(item.get_text())
+    foodList.pop(-1)
+    return foodList 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
